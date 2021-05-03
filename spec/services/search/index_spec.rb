ff9db58d3a38c678f97e97c5b276ebf2151e77e2
Matt Wolfe <m5wolfe@uwaterloo.ca>
2021-05-03 12:06:03 -0400

    testing update

--- spec/services/search/index_spec.rb
+++ spec/services/search/index_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Search::Index do
-    let(:img_row) { FactoryBot.create(:image_row) }
 
     describe 'index_one' do
         it 'should create an index record' do
-            before_count = ImageSearchIndex.count
-            expect(before_count).to eq(0)
+            img_row = FactoryBot.create(:image_row)
+            expect(ImageSearchIndex.count).to eq(0)
             described_class.index_one(img_row)
             expect(ImageSearchIndex.count).to eq(1)
 
@@ -19,28 +18,36 @@ RSpec.describe Search::Index do
             expect(indexed_row.byte_size).to eq(38411)
         end
         it 'should update an index record' do
-            before_count = ImageSearchIndex.count
-            expect(before_count).to eq(0)
+            img_row = FactoryBot.create(:image_row)
+            # index it
             described_class.index_one(img_row)
             expect(ImageSearchIndex.count).to eq(1)
-
-            # TODO: test me by updating with different image!
+            indexed_row = ImageSearchIndex.find_by(record_id: img_row.id)
+            expect(indexed_row.width).to eq(544)
+            # make change
+            img_row.image.metadata["width"] = 900
+            img_row.save
+            described_class.index_one(img_row)
+            # reindex it
+            indexed_row = ImageSearchIndex.find_by(record_id: img_row.id)
+            expect(indexed_row.width).to eq(900)
         end
     end
 
-    describe 'un_index_one' do
-        # it 'should remove the index for a record' do
-        #     indexed_row = ImageSearchIndex.find_by(record_id: img_row.id)
-        #     expect(indexed_row).to be_nil
-        #
-        #     described_class.index_one(img_row)
-        #     indexed_row = ImageSearchIndex.find_by(record_id: img_row.id)
-        #     expect(indexed_row).to_not be_nil
-        #
-        #     described_class.un_index_one(img_row)
-        #     indexed_row = ImageSearchIndex.find_by(record_id: img_row.id)
-        #     expect(indexed_row).to be_nil
-        # end
-    end
+    # describe 'un_index_one' do
+    #     it 'should remove the index for a record' do
+    #         # index it
+    #         expect(ImageSearchIndex.count).to eq(0)
+    #         described_class.index_one(img_row)
+    #         expect(ImageSearchIndex.count).to eq(1)
+    #         indexed_row = ImageSearchIndex.find_by(record_id: img_row.id)
+    #         expect(indexed_row).to_not be_nil
+    #         # un index it
+    #         described_class.un_index_one(img_row)
+    #         expect(ImageSearchIndex.count).to eq(0)
+    #         indexed_row = ImageSearchIndex.find_by(record_id: img_row.id)
+    #         expect(indexed_row).to be_nil
+    #     end
+    # end
 
 end
